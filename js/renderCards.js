e78c96b751d0abe5de621d700d5515c17f4f2524
parth1007 <parthsoni03ps@gmail.com>
2023-10-26 06:56:57 +0530

    zxcvbnm

--- js/renderCards.js
+++ js/renderCards.js
@@ -15,54 +15,24 @@ export function renderCards(container) {
   else if(container == "classesContainer")
     data = classesData
 
-  for (var i = 0; i < data.length; i++) {
-      var card = document.createElement('div');
-      card.className = 'col-lg-4 col-md-4 single-feature';
+    for (var i = 0; i < data.length; i++) {
+        var card = `
+          <div class="col-lg-4 col-md-4 single-feature" data-toggle="modal" data-target="#courseModal-${i}">
+            <figure>
+              <img class="img-fluid" src="${data[i].img}" alt="">
+              <div class="overlay overlay-bg"></div>
+            </figure>
+            <div class="text-center">
+              <h4 class="mb-10">${data[i].title}</h4>
+              <p1>${data[i].p1}</p1><br>
+              <p2>${data[i].p2}</p2><br>
+              <p3>${data[i].p3}</p3>
+            </div>
+          </div>
+        `;
 
-      var figure = document.createElement('figure');
-
-      var image = document.createElement('img');
-      image.className = 'img-fluid';
-      image.src = data[i].img;
-      image.alt = '';
-
-      var overlay = document.createElement('div');
-      overlay.className = 'overlay overlay-bg';
-
-      figure.appendChild(image);
-      figure.appendChild(overlay);
-
-      var cardText = document.createElement('div');
-      cardText.className = 'text-center';
-
-      var title = document.createElement('h4');
-      title.className = 'mb-10';
-      title.textContent = data[i].title;
-
-      var p1 = document.createElement('p1');
-      p1.innerHTML = data[i].p1;
-
-      var p2 = document.createElement('p2');
-      p2.innerHTML = data[i].p2;
-
-      var p3 = document.createElement('p3');
-      p3.innerHTML = data[i].p3;
-
-
-      cardText.appendChild(title);
-      cardText.appendChild(p1);
-      cardText.appendChild(document.createElement('br'));
-      cardText.appendChild(p2);
-      cardText.appendChild(document.createElement('br'));
-      cardText.appendChild(p3);
-
-      card.appendChild(figure);
-      card.appendChild(cardText);
-
-      cardContainer.appendChild(card);
-
-      
-  }
+        cardContainer.innerHTML += card;
+      }
 }
 
 
--- js/renderCourse.js
+++ js/renderCourse.js
@@ -1,14 +1,23 @@
 import courseData from '../data/courses.json' assert { type: "json" };
 
 
-export function renderTeamModal() {
+export function renderCourseModal() {
 
     var container = document.getElementsByTagName("BODY")[0];
 
     for (var i = 0; i <courseData.length; i++){
         var htmlContent = `
             <div class="modal fade" id="courseModal-${i}" role="dialog" aria-labelledby="donateModalTitle" aria-hidden="true" data-backdrop="true">
-                ${courseData[i].title}
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title" id="donateModalLongTitle">${courseData[i].title}</h3>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         `;
         container.innerHTML += htmlContent;
