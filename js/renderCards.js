f82bffc72f6e796419c8ff91ea814e6f5a665c8a
parth1007 <parthsoni03ps@gmail.com>
2023-11-21 01:15:41 +0530

    main-page-color

--- js/renderCards.js
+++ js/renderCards.js
@@ -36,26 +36,82 @@ export function renderCards(container) {
 
 
 
+// export function renderTestimonialCard() {
+//   var container = document.getElementById("testimonialContainer");
+
+
+//   for (var i = 0; i < testimonialData.length; i++) {
+//     var card = `
+//         <div class="single-testimonial item">
+//             <div class="feedback-text">${testimonialData[i].feedback}<div>
+//             <h4 class="desc">${testimonialData[i].person}</h4>
+//         </div>
+//       `;
+    
+//       var containerDiv = document.createElement("div");
+
+//       containerDiv.innerHTML = card;
+
+//     container.appendChild(containerDiv);
+
+//   }
+// }
+
+
+
+
 export function renderTestimonialCard() {
   var container = document.getElementById("testimonialContainer");
 
-
   for (var i = 0; i < testimonialData.length; i++) {
-    var card = `
-        <div class="single-testimonial item">
-            <div class="feedback-text">${testimonialData[i].feedback}<div>
-            <h4 class="desc">${testimonialData[i].person}</h4>
-        </div>
-      `;
+    var feedbackText = testimonialData[i].feedback;
+    var truncatedFeedback = truncateText(feedbackText, 45); 
     
-      var containerDiv = document.createElement("div");
+    var card = `
+      <div class="single-testimonial item">
+        
+        <div class="feedback-person">
+            <div class="test-avatar">${testimonialData[i].person[0]}</div>
+            <div class="feedback-name"> 
+                <h4 class="desc">${testimonialData[i].person}</h4>
+                <div class="stars"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#0011ff}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#0011ff}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#0011ff}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#0011ff}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#0011ff}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                </div>
+            </div>
+        
+        </div>
+        
+        <div class="feedback-text" id="feedbackText${i}">${truncatedFeedback}</div>
+        <img class="quote-vec" src="img/quote.png">
+      </div>
+        
 
-      containerDiv.innerHTML = card;
 
+    `;
+
+    var containerDiv = document.createElement("div");
+    containerDiv.innerHTML = card;
     container.appendChild(containerDiv);
-
   }
+
 }
+
+function truncateText(text, maxWords) {
+  var words = text.split(' ').slice(0, maxWords);
+  var truncatedFeedback =  words.join(' ');
+
+  var hasMoreText = text.split(' ').length > truncatedFeedback.split(' ').length;
+
+  if(hasMoreText) truncatedFeedback += `<span class="read-more-btn"> ... Read more</span>`;
+
+  return truncatedFeedback;
+}
+
+
 
 
 
